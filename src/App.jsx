8cc8c0cdc9a12a092853f0f11899a49a543e65a6
edgardo <e.perez1418@outloo.com>
2025-05-22 22:47:59 -0400

    ran cognito first

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { SalesForm } from './components/SalesForm';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { SalesForm } from "./components/SalesForm";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 function App() {
   return (
-    <div className='App'>
-      <SalesForm />
+    <div className="App">
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <div style={{ padding: "20px", textAlign: "center" }}>
+              <h2>Welcome, {user.username}!</h2>
+              <button onClick={signOut} style={{ marginBottom: "20px" }}>
+                Sign out
+              </button>
+            </div>
+            <SalesForm />
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Amplify } from "aws-amplify";
 
-createRoot(document.getElementById('root')).render(
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "us-east-1_n84YveCLs",
+      userPoolClientId: "5sovu2fdflgvrur96sa30vjiel",
+      loginWith: {
+        oauth: {
+          domain: "your-cognito-domain.auth.us-east-1.amazoncognito.com",
+          scopes: ["openid", "email", "profile"],
+          redirectSignIn: ["https://d84l1y8p4kdic.cloudfront.net"],
+          redirectSignOut: ["https://d84l1y8p4kdic.cloudfront.net"],
+          responseType: "code",
+        },
+      },
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
