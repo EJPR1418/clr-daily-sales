7257f800135e8d3245f0d45b55452332ebe3099c
edgardo <e.perez1418@outloo.com>
2025-05-26 21:50:39 -0400

    adding auth files amplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { SalesForm } from './components/SalesForm';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import { SalesForm } from "./components/SalesForm";
+
+Amplify.configure(outputs);
 
 function App() {
   return (
-    <div className='App'>
-      <SalesForm />
+    <div className="App">
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <div style={{ padding: "20px", textAlign: "center" }}>
+              <h2>Welcome, {user.username}!</h2>
+              <button onClick={signOut} style={{ marginBottom: "20px" }}>
+                Sign out
+              </button>
+            </div>
+            <SalesForm />
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
